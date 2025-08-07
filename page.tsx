ef8500855aec2655068c687c10616b31e0e842c3
mahmoud16-42 <laptoplap35@gmail.com>
2025-08-07 19:04:08 +0300

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>✅ المشروع شغال بنجاح!</h1>
+      <p>مرحبا بك في تطبيق كتابة الإيميلات بالذكاء الاصطناعي.</p>
+    </main>
+  );
+}
